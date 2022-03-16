4d15736ead5f029cf843c26c29ef4d3488700263
Lujain <82683327+LujainAlajmi@users.noreply.github.com>
2022-03-16 12:02:59 +0300

    advice app

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      xs: "375px",
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        LightCyan: "hsl(193, 38%, 86%)",
+        NeonGreen: "hsl(150, 100%, 66%)",
+        GrayishBlue: "hsl(217, 19%, 38%)",
+        DarkGrayishBlue: "hsl(217, 19%, 24%)",
+        DarkBlue: "hsl(218, 23%, 16%)",
+      },
+    },
+  },
+  plugins: [],
+};
